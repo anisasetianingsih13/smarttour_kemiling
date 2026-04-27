e911542c697174b44cafc98698f3537786d6c16b
Apriliya Ananta <apriliya_ananta@teknokrat.ac.id>
2026-04-27 15:39:36 +0700

    style: update theme dan routing

--- user/lib/main.dart
+++ user/lib/main.dart
@@ -13,10 +13,32 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'SmartTour Kemiling',
+
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: const Color(0xFF6DB193), // 🌿 warna soft green
+          brightness: Brightness.light,
+        ),
+
+        scaffoldBackgroundColor: const Color(0xFFF5F7F6), // background soft
+
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+          centerTitle: true,
+          titleTextStyle: TextStyle(
+            color: Colors.black,
+            fontSize: 18,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+
+        textTheme: const TextTheme(
+          bodyMedium: TextStyle(fontSize: 14),
+        ),
       ),
-      home: HomeScreen(), // 🔥 ini yang penting
+
+      home: HomeScreen(),
     );
   }
 }
