bdb8e8e1919e7311f3216a1b5423dacc37be495f
Apriliya Ananta <apriliya_ananta@teknokrat.ac.id>
2026-04-27 10:15:38 +0700

    feat: menambahkan model tourism place

---
+++ user/lib/models/tourism_place.dart
@@ -0,0 +1,15 @@
+class TourismPlace {
+  final String name;
+  final String category;
+  final double rating;
+  final String imageUrl;
+  final bool isIndoor;
+
+  TourismPlace({
+    required this.name,
+    required this.category,
+    required this.rating,
+    required this.imageUrl,
+    required this.isIndoor,
+  });
+}
