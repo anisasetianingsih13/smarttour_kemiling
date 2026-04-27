ec71a33a44fb2d1b6f92a2d45f0e95786ab21894
Apriliya Ananta <apriliya_ananta@teknokrat.ac.id>
2026-04-27 10:33:54 +0700

    feat: menambahkan dummy data wisata

---
+++ user/lib/data/dummy_data.dart
@@ -0,0 +1,25 @@
+import '../models/tourism_place.dart';
+
+List<TourismPlace> dummyPlaces = [
+  TourismPlace(
+    name: "Cafe Kemiling",
+    category: "Kuliner",
+    rating: 4.5,
+    imageUrl: "https://via.placeholder.com/150",
+    isIndoor: true,
+  ),
+  TourismPlace(
+    name: "Bukit Sakura",
+    category: "Wisata Alam",
+    rating: 4.7,
+    imageUrl: "https://via.placeholder.com/150",
+    isIndoor: false,
+  ),
+  TourismPlace(
+    name: "Taman Kupu-Kupu",
+    category: "Edukasi",
+    rating: 4.3,
+    imageUrl: "https://via.placeholder.com/150",
+    isIndoor: false,
+  ),
+];
