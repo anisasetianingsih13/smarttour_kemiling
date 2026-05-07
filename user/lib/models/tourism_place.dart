300a0100c7c164275011bbc34a4fe4f7c8859fe4
Apriliya Ananta <apriliya_ananta@teknokrat.ac.id>
2026-05-07 15:52:12 +0700

    fitur: menambahkan atribut koordinat dan kategori pada model TourismPlace

--- user/lib/models/tourism_place.dart
+++ user/lib/models/tourism_place.dart
@@ -4,6 +4,8 @@ class TourismPlace {
   final double rating;
   final String imageUrl;
   final bool isIndoor;
+  final double lat;
+  final double lng;
 
   TourismPlace({
     required this.name,
@@ -11,5 +13,7 @@ class TourismPlace {
     required this.rating,
     required this.imageUrl,
     required this.isIndoor,
+    required this.lat, // Tambahkan di constructor
+    required this.lng, // Tambahkan di constructor
   });
 }
