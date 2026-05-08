ce67403d022a2235603e0494f0b68ad9071fcbd6
anisasetianingsih13 <anisasetianingsih793@gmail.com>
2026-05-08 20:24:26 +0700

    Menambahkan PrismaService ke TourismModule

--- api/tourism-service/src/tourism/tourism.module.ts
+++ api/tourism-service/src/tourism/tourism.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { TourismService } from './tourism.service';
 import { TourismController } from './tourism.controller';
+import { PrismaService } from '../prisma.service';
 
 @Module({
   controllers: [TourismController],
-  providers: [TourismService],
+  providers: [TourismService, PrismaService],
 })
 export class TourismModule {}
