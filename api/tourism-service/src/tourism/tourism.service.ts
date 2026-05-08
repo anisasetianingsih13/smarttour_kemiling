17affa1b7a77a37cbf8bbbc96f3f6179fb161c19
anisasetianingsih13 <anisasetianingsih793@gmail.com>
2026-05-08 19:10:30 +0700

    Membuat fitur get all data tourism

--- api/tourism-service/src/tourism/tourism.service.ts
+++ api/tourism-service/src/tourism/tourism.service.ts
@@ -1,15 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma.service';
 import { CreateTourismDto } from './dto/create-tourism.dto';
 import { UpdateTourismDto } from './dto/update-tourism.dto';
 
 @Injectable()
 export class TourismService {
+  constructor(private prisma: PrismaService) {}
   create(createTourismDto: CreateTourismDto) {
     return 'This action adds a new tourism';
   }
 
-  findAll() {
-    return `This action returns all tourism`;
+  async findAll() {
+    return await this.prisma.tourismPlace.findMany();
   }
 
   findOne(id: number) {
