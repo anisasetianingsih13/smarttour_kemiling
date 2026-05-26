c672a9ee0280b4c3de50466f692fd39271fb7072
anisasetianingsih13 <anisasetianingsih793@gmail.com>
2026-05-26 15:18:20 +0700

    Mengubah konfigurasi port dan menambahkan global prefix API

--- api/tourism-service/src/main.ts
+++ api/tourism-service/src/main.ts
@@ -3,6 +3,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3001);
+
+  // tambahkan prefix api
+  app.setGlobalPrefix('api');
+
+  // jalankan server berdasarkan env
+  await app.listen(process.env.PORT!);
 }
+
 void bootstrap();
