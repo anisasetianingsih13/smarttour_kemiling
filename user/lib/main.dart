3d5c0e07e50c0a6a04e3426e64ebf88933f7f128
apriliyaananta <apriliya_ananta@teknokrat.ac.id>
2026-06-09 12:59:57 +0700

    feat(user): mengatur routing navigasi dan inisialisasi penyimpanan lokal

--- user/lib/main.dart
+++ user/lib/main.dart
@@ -1,15 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'screens/home_screen.dart';
+import 'screens/onboarding_screen.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+
+  await Hive.initFlutter();
+  await Hive.openBox<String>('favorites');
+
+  final prefs = await SharedPreferences.getInstance();
+  final bool isFirstTime = prefs.getBool('isFirstTime') ?? true;
+
+  runApp(MyApp(isFirstTime: isFirstTime));
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  final bool isFirstTime;
+  const MyApp({super.key, required this.isFirstTime});
 
   @override
   Widget build(BuildContext context) {
+    FlutterNativeSplash.remove();
+
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'SmartTour Kemiling',
@@ -38,7 +54,7 @@ class MyApp extends StatelessWidget {
         ),
       ),
 
-      home: HomeScreen(),
+      home: isFirstTime ? const OnboardingScreen() : const HomeScreen(),
     );
   }
 }
