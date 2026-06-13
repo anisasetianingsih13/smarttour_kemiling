baf74f41cef397bd729a527349fadd403fa07ec2
anisasetianingsih13 <anisasetianingsih793@gmail.com>
2026-06-13 21:44:05 +0700

    feat(tourism): menambahkan resource tourism pada gateway

--- api/gateway/src/app.module.ts
+++ api/gateway/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TourismModule } from './tourism/tourism.module';
 
 @Module({
-  imports: [],
+  imports: [TourismModule],
   controllers: [AppController],
   providers: [AppService],
 })
